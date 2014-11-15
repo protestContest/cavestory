1714f438d6c0455d69ff67e9e674a39c5eaefd56
Zack Michener <zack@zackmichener.net>
2014-11-15 11:22:09 -0800

    add colliding tiles!

--- src/map.cc
+++ src/map.cc
@@ -2,6 +2,7 @@
 #include "sprite.h"
 #include "graphics.h"
 #include "game.h"
+#include "rectangle.h"
 
 using boost::shared_ptr;
 using std::vector;
@@ -11,9 +12,9 @@ Map* Map::createTestMap(Graphics& graphics) {
 
     const int num_rows = 15;
     const int num_cols = 20;
-    map->foreground_sprites_ = vector<vector<shared_ptr<Sprite> > >(
-            num_rows, vector<shared_ptr<Sprite> >(
-                num_cols, shared_ptr<Sprite>()));
+    map->tiles_ = vector<vector<Tile> >(
+            num_rows, vector<Tile>(
+                num_cols, Tile()));
 
     shared_ptr<Sprite> sprite(new Sprite(
                 graphics,
@@ -21,34 +22,41 @@ Map* Map::createTestMap(Graphics& graphics) {
                 Game::kTileSize, 0,
                 Game::kTileSize, Game::kTileSize));
 
+    Tile tile(WALL_TILE, sprite);
     const int row = 11;
     for (int col = 0; col < num_cols; ++col) {
-        map->foreground_sprites_[row][col] = sprite;
+        map->tiles_[row][col] = tile;
     }
-    map->foreground_sprites_[10][5] = sprite;
-    map->foreground_sprites_[9][4] = sprite;
-    map->foreground_sprites_[8][3] = sprite;
-    map->foreground_sprites_[7][2] = sprite;
-    map->foreground_sprites_[10][1] = sprite;
+    map->tiles_[10][5] = tile;
+    map->tiles_[9][4] = tile;
+    map->tiles_[8][3] = tile;
+    map->tiles_[7][2] = tile;
+    map->tiles_[10][1] = tile;
 
     return map;
 }
 
 void Map::update(int elapsed_time_ms) {
-    for (size_t row = 0; row < foreground_sprites_.size(); ++row) {
-        for (size_t col = 0; col < foreground_sprites_[row].size(); ++col) {
-            if (foreground_sprites_[row][col]) {
-                foreground_sprites_[row][col]->update(elapsed_time_ms);
+    for (size_t row = 0; row < tiles_.size(); ++row) {
+        for (size_t col = 0; col < tiles_[row].size(); ++col) {
+            if (tiles_[row][col].sprite) {
+                tiles_[row][col].sprite->update(elapsed_time_ms);
             }
         }
     }
 }
 
+std::vector<Map::CollisionTile> Map::getCollidingTiles(const Rectangle& rectangle) const {
+    vector<CollisionTile> collision_tiles;
+
+    return collision_tiles;
+}
+
 void Map::draw(Graphics& graphics) const {
-    for (size_t row = 0; row < foreground_sprites_.size(); ++row) {
-        for (size_t col = 0; col < foreground_sprites_[row].size(); ++col) {
-            if (foreground_sprites_[row][col]) {
-                foreground_sprites_[row][col]->draw(
+    for (size_t row = 0; row < tiles_.size(); ++row) {
+        for (size_t col = 0; col < tiles_[row].size(); ++col) {
+            if (tiles_[row][col].sprite) {
+                tiles_[row][col].sprite->draw(
                         graphics, col * Game::kTileSize, row * Game::kTileSize);
             }
         }
--- src/map.h
+++ src/map.h
@@ -6,16 +6,43 @@
 
 struct Graphics;
 struct Sprite;
+struct Rectangle;
 
 class Map {
 public:
+    enum TileType {
+        AIR_TILE,
+        WALL_TILE
+    };
+
+    struct CollisionTile {
+        CollisionTile(int row, int col, TileType tile_type) :
+            row(row),
+            col(col),
+            tile_type(tile_type) {}
+        TileType tile_type;
+        int row, col;
+    };
+
     static Map* createTestMap(Graphics& graphics);
+
+    std::vector<CollisionTile> getCollidingTiles(const Rectangle& rectangle) const;
 
     void update(int elapsed_time_ms);
     void draw(Graphics& graphics) const;
 
 private:
-    std::vector<std::vector<boost::shared_ptr<Sprite> > > foreground_sprites_;
+    struct Tile {
+        Tile(TileType tile_type = AIR_TILE,
+            boost::shared_ptr<Sprite> sprite=boost::shared_ptr<Sprite>()):
+                tile_type(tile_type),
+                sprite(sprite) {}
+
+        TileType tile_type;
+        boost::shared_ptr<Sprite> sprite;
+    };
+
+    std::vector<std::vector<Tile> > tiles_;
 };
 
 #endif
