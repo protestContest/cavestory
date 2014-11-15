44d1b925ad126138be4e8b8f46c465da0eef0f08
Zack Michener <zack@zackmichener.net>
2014-11-14 22:30:46 -0800

    implement multiple sprites!

--- src/player.h
+++ src/player.h
@@ -1,7 +1,8 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
-#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+#include <map>
 #include "sprite.h"
 
 struct Sprite;
@@ -19,10 +20,35 @@ public:
     void stopMoving();
 
 private:
+    enum MotionType {
+        STANDING,
+        WALKING,
+    };
+    enum HorizontalFacing {
+        LEFT,
+        RIGHT,
+    };
+
+    struct SpriteState {
+        SpriteState(MotionType motion_type=STANDING,
+                    HorizontalFacing horizontal_facing=LEFT):
+            motion_type(motion_type),
+            horizontal_facing(horizontal_facing) {}
+
+        MotionType motion_type;
+        HorizontalFacing horizontal_facing;
+    };
+
+    SpriteState getSpriteState();
+    void initializeSprites(Graphics& graphics);
+
+    friend bool operator<(const SpriteState& a, const SpriteState& b);
+
     int x_, y_;
-    boost::scoped_ptr<Sprite> sprite_;
+    std::map< SpriteState, boost::shared_ptr<Sprite> > sprites_;
     float velocity_x_;
     float acceleration_x_;
+    HorizontalFacing horizontal_facing_;
 };
 
 #endif
